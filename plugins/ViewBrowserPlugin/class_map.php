03283f992d0b3608975f04d859baf246197b9331
Duncan Cameron <phplist@dcameron.me.uk>
2015-11-06 11:55:17 +0000

    Added class map

---
+++ plugins/ViewBrowserPlugin/class_map.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'phpList\plugin\ViewBrowserPlugin\ContentCreator' => $base . '/ViewBrowserPlugin/ContentCreator.php',
+    'phpList\plugin\ViewBrowserPlugin\ContentDocument' => $base . '/ViewBrowserPlugin/ContentDocument.php',
+    'phpList\plugin\ViewBrowserPlugin\DAO' => $base . '/ViewBrowserPlugin/DAO.php',
+);
