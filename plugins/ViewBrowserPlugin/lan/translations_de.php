b636c497ac99422e8ed0835d66830600b21b74f1
Thomas Gottfried <thomas.gottfried@gmx.de>
2018-10-15 18:37:23 +0200

    German translation, resolves #20

---
+++ plugins/ViewBrowserPlugin/lan/translations_de.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    ['A numeric message id must be specified', 'Eine nummerische Nachrichten ID muss angegeben werden'],
+    ['A numeric template image id must be specified', 'Eine nummerische Template ID muss angegeben werden'],
+    ['A user uid must be specified', 'Eine User uid muss angegeben werden'],
+    ['Additional attributes for the html &lt;a> element', 'Zusätzliches Attribut für das &lt;a> Element'],
+    ['Campaign archive', 'Nachrichen Archiv'],
+    ['Campaigns sent to %s', 'Nachrichten gesendet an %s'],
+    ['CommonPlugin must be installed', 'CommonPlugin muss installiert sein'],
+    ['Message with id %d does not exist', 'Nachricht mit der id %d existiert nicht'],
+    ['No campaigns found', 'Keine Nachrichten gefunden'],
+    ['Plugins to be used when creating the email. Usually leave this unchanged.', 'Zum Erstellen von emails verwendete Plugins. Üblicherweise können die Angaben unverändert bleiben.'],
+    ['The number of campaigns to display on each archive page (between %d and %d)', 'Anzahl Nachrichten auf einer Archivseite (zwischen %d und %d)'],
+    ['The text of the archive link', 'Text für Archiv Link'],
+    ['The text of the link', 'Text für Nachrichten Link'],
+    ['The xsl extension must be installed', 'Die xsl extension muss installiert sein'],
+    ['URL of custom CSS file', 'URL des custom CSS file'],
+    ['Unable to retrieve URL %s', 'URL %s kann nicht aufgerufen werden'],
+    ['User with uid %s does not exist', 'User mit der uid %s existiert nicht'],
+    ['View in browser', 'Im Browser anzeigen'],
+    ['Whether the plugin should provide an anonymous page', 'Wird eine anonyme Nachrichtenseite zur Verfügung gestellt'],
+    ['email archive', 'Email Archiv'],
+];
